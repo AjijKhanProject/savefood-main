8b7ed61eac7254d7134d36af7574cb22d8dc5f6f
AjijKhanProject <ajijkhaan.project@gmail.com>
2022-03-14 23:06:00 +0600

    chagne shadow

--- components/Home.js
+++ components/Home.js
@@ -1,78 +1,114 @@
+import firestore from '@react-native-firebase/firestore';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {TouchableOpacity} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import Profile from './User/Profile';
+import Contact from './User/Contact';
 import Donate from './User/Donate';
-import Contact from './User/Contact'
-import UserHome from './User/UserHome'
-import Volunteer from './User/Volunteer'
-import {TouchableOpacity,StyleSheet,View} from 'react-native'
-import firestore from '@react-native-firebase/firestore'
+import Profile from './User/Profile';
+import UserHome from './User/UserHome';
+import Volunteer from './User/Volunteer';
 
+const Home = props => {
+  const Tab = createBottomTabNavigator();
+  const [UserInformation, setUserInformation] = React.useState(null);
+  const params = props.route.params;
+  React.useEffect(() => {
+    firestore()
+      .collection('UserInformation')
+      .doc(params.uid)
+      .onSnapshot(data => {
+        setUserInformation(data.data());
+      });
+  }, []);
+  const header = ({navigation}) => ({
+    headerLeft: () => (
+      <TouchableOpacity
+        style={{marginHorizontal: 10}}
+        onPress={() => {
+          navigation.navigate('Rank List', {uid: params.uid});
+        }}>
+        <Ionicons name="trophy-sharp" size={35} color="#F39C12" />
+      </TouchableOpacity>
+    ),
+    headerRight: () => (
+      <TouchableOpacity
+        style={{marginHorizontal: 10}}
+        onPress={() => {
+          navigation.navigate('Notification', {uid: params.uid});
+        }}>
+        <Ionicons
+          style={{marginLeft: 80}}
+          name="notifications-sharp"
+          size={35}
+          color="#F39C12"
+        />
+      </TouchableOpacity>
+    ),
+    headerTitleAlign: 'center',
+  });
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
 
-const Home = (props) => {
-    const Tab = createBottomTabNavigator();
-    const [UserInformation,setUserInformation]= React.useState(null)
-    const params=props.route.params
-    React.useEffect(()=>{
-        firestore().collection('UserInformation').doc(params.uid).onSnapshot(data=>{
-            setUserInformation(data.data())
-        })
-    },[])
-    const header=({navigation})=>({
-        headerLeft: () => (
-            <TouchableOpacity style={{marginHorizontal:10}} onPress={() =>{
-                navigation.navigate('Rank List',{uid:params.uid})
-            }}>
-                <Ionicons name="ios-trophy" size={30} color="#F39C12" />
-            </TouchableOpacity>
-        ),
-        headerRight: () => (
-            <TouchableOpacity style={{marginHorizontal:10}} onPress={() =>{
-                navigation.navigate('Notification',{uid:params.uid})
-            }}>
-                <Ionicons style={{ marginLeft: 80 }} name="md-notifications" size={30} color="#F39C12" />
-            </TouchableOpacity>
-        ),
-        headerTitleAlign:'center',
-    })
-        return (
-            <Tab.Navigator screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-    
-                    if (route.name === 'User Home') {
-                        iconName = focused
-                            ? 'home'
-                            : 'home-outline';
-    
-                    } else if (route.name === 'Profile') {
-                        iconName = focused ? 'person-circle-sharp' : 'person-circle-outline';
-                    } else if (route.name === 'Donate') {
-                        iconName = focused ? 'pizza' : 'pizza-outline';
-                    } else if (route.name === 'Volunteer') {
-                        iconName = focused ? 'walk-outline' : 'walk-sharp';
-                    } else if (route.name === 'Contact') {
-                        iconName = focused ? 'ios-call' : 'ios-call-outline';
-                    }
-    
-                    // You can return any component that you like here!
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                },
-                tabBarActiveTintColor: '#F39C12',
-                tabBarInactiveTintColor: 'gray',
-            })} >
-                <Tab.Screen name="User Home" component={UserHome} options={({navigation})=>header({navigation})}/>
-                <Tab.Screen name="Profile" component={Profile} initialParams={{uid:params.uid}} options={({navigation})=>header({navigation})} />
-                <Tab.Screen name="Donate" component={Donate} initialParams={{
-                    user:UserInformation,uid:params.uid
-                }}  options={({navigation})=>header({navigation})}/>
-                <Tab.Screen name="Volunteer" component={Volunteer} initialParams={{
-                    user:UserInformation,uid:params.uid
-                }}  options={({navigation})=>header({navigation})}/>
-                <Tab.Screen name="Contact" component={Contact}  options={({navigation})=>header({navigation})}/>
-            </Tab.Navigator>
-        );
+          if (route.name === 'User Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused
+              ? 'person-circle-sharp'
+              : 'person-circle-outline';
+          } else if (route.name === 'Donate') {
+            iconName = focused ? 'pizza' : 'pizza-outline';
+          } else if (route.name === 'Volunteer') {
+            iconName = focused ? 'walk-outline' : 'walk-sharp';
+          } else if (route.name === 'Contact') {
+            iconName = focused ? 'ios-call' : 'ios-call-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#F39C12',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen
+        name="User Home"
+        component={UserHome}
+        options={({navigation}) => header({navigation})}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        initialParams={{uid: params.uid}}
+        options={({navigation}) => header({navigation})}
+      />
+      <Tab.Screen
+        name="Donate"
+        component={Donate}
+        initialParams={{
+          user: UserInformation,
+          uid: params.uid,
+        }}
+        options={({navigation}) => header({navigation})}
+      />
+      <Tab.Screen
+        name="Volunteer"
+        component={Volunteer}
+        initialParams={{
+          user: UserInformation,
+          uid: params.uid,
+        }}
+        options={({navigation}) => header({navigation})}
+      />
+      <Tab.Screen
+        name="Contact"
+        component={Contact}
+        options={({navigation}) => header({navigation})}
+      />
+    </Tab.Navigator>
+  );
 };
 
 export default Home;
--- components/Post.js
+++ components/Post.js
@@ -1,100 +1,105 @@
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 import React from 'react';
-import { Alert, FlatList, StyleSheet, Dimensions, SafeAreaView, ScrollView, Text, View } from 'react-native';
-import Color from './Styles/Color'
+import {
+  Dimensions,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import AnimatedLoader from 'react-native-animated-loader';
+import {FAB} from 'react-native-paper';
+import SplashScreen from 'react-native-splash-screen';
+import Icon from 'react-native-vector-icons/AntDesign';
 import Cart from './cart/Cart';
-import { FAB } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/AntDesign'
-import AnimatedLoader from "react-native-animated-loader";
-import model from './Styles/model'
-import auth from '@react-native-firebase/auth'
-import SplashScreen from 'react-native-splash-screen'
-import firestore from '@react-native-firebase/firestore'
+import model from './Styles/model';
 
-const Post = (props) => {
-    const window = Dimensions.get('window');
-    const navigation = props.navigation;
-    const [data, setData] = React.useState([])
-    const [User, setUser] = React.useState(null);
+const Post = props => {
+  const window = Dimensions.get('window');
+  const navigation = props.navigation;
+  const [data, setData] = React.useState([]);
+  const [User, setUser] = React.useState(null);
 
-
-    React.useEffect(() => {
-        auth().onAuthStateChanged((user) => {
-            if (user) {
-                setUser(user);
-                navigation.navigate('Home', { email: user.email, uid: user.uid })
-            }
-        })
-        SplashScreen.hide();
-    }, [])
-    React.useEffect(() => {
-         firestore().collection('Post').orderBy('NewDate', 'desc').onSnapshot((data) => {
-              if (data) {
-                  let arr = []
-                  data.forEach((item) => {
-                      arr.push(item.data())
-                  })
-                  setData(arr)
-              } else {
-                  setData([])
-              }
-          })
-    },[])
-    const styles = StyleSheet.create({
-        view: {
-            marginTop: 0,
-            marginBottom: 0,
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: window.width,
-            height: window.height
-
-        },
-        fab: {
-            position: 'absolute',
-            margin: 16,
-            right: 10,
-            bottom: 10,
-            width: 140,
-            height: 40,
-            backgroundColor: '#F39C12',
-            justifyContent: 'center',
-            alignItems: 'center',
-            color: '#FFFFFF',
-
+  React.useEffect(() => {
+    auth().onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+        navigation.navigate('Home', {email: user.email, uid: user.uid});
+      }
+    });
+    SplashScreen.hide();
+  }, []);
+  React.useEffect(() => {
+    firestore()
+      .collection('Post')
+      .orderBy('NewDate', 'desc')
+      .onSnapshot(data => {
+        if (data) {
+          let arr = [];
+          data.forEach(item => {
+            arr.push(item.data());
+          });
+          setData(arr);
+        } else {
+          setData([]);
         }
-    })
+      });
+  }, []);
+  //default style of post
+  const styles = StyleSheet.create({
+    view: {
+      marginTop: 0,
+      marginBottom: 0,
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: window.width,
+      height: window.height,
+    },
+    fab: {
+      position: 'absolute',
+      margin: 16,
+      right: 10,
+      bottom: 10,
+      width: 140,
+      height: 40,
+      backgroundColor: '#F39C12',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: '#FFFFFF',
+    },
+  });
 
-    return (
-        <SafeAreaView style={styles.view}>
-            <ScrollView>
-                {
-                    data ? (
-                        data.length > 0 ? (
-                            data.map(data => (
-                                <Cart key={data.Id} data={data} />
-                            ))
-                        ) : (
-                            <Text style={{ marginTop: 100 }}>No Data Available</Text>
-                        )
-                    ) : (
-                        <AnimatedLoader
-                            visible={true}
-                            overlayColor="rgba(255, 255, 255, 0.459)"
-                            source={require("./Files/88967-food-delivery-service.json")}
-                            animationStyle={model.loader}
-                            speed={1}
-                        >
-                            <Text style={{ color: "black" }}>Loading...</Text>
-                        </AnimatedLoader>
-                    )
-                }
-            </ScrollView>
-            <FAB style={styles.fab} icon={() => (
-                <Icon name="login" color='#ffff' size={20} />)} label='Log In' onPress={() => {
-                    navigation.navigate('LogIn');
-                }} />
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.view}>
+      <ScrollView>
+        {data ? (
+          data.length > 0 ? (
+            data.map(data => <Cart key={data.Id} data={data} />)
+          ) : (
+            <Text style={{marginTop: 100}}>No Data Available</Text>
+          )
+        ) : (
+          <AnimatedLoader
+            visible={true}
+            overlayColor="rgba(255, 255, 255, 0.459)"
+            source={require('./Files/88967-food-delivery-service.json')}
+            animationStyle={model.loader}
+            speed={1}>
+            <Text style={{color: 'black'}}>Loading...</Text>
+          </AnimatedLoader>
+        )}
+      </ScrollView>
+      <FAB
+        style={styles.fab}
+        icon={() => <Icon name="login" color="#ffff" size={20} />}
+        label="Log In"
+        onPress={() => {
+          navigation.navigate('LogIn');
+        }}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default Post;
