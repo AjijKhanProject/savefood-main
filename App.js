221f27fe44fab86c176bece7630ef2005be6bd9c
AjijKhanProject <ajijkhaan.project@gmail.com>
2022-03-14 21:37:02 +0600

    eslint

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
-import { LogBox, Text } from 'react-native';
-import AnimatedLoader from "react-native-animated-loader";
-import model from './components/Styles/model';
-import LogIn from './components/LogIn'
-import SignUp from './components/SignUp'
-import Forget from './components/Forget'
-import Home from './components/Home'
-import RankList from './components/User/RankList'
-import Notification from './components/User/Notification'
-import Post from './components/Post'
-import { Avatar } from 'react-native-paper'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-LogBox.ignoreAllLogs()
-
+import {LogBox} from 'react-native';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import Forget from './components/Forget';
+import Home from './components/Home';
+import LogIn from './components/LogIn';
+import Post from './components/Post';
+import SignUp from './components/SignUp';
+import Notification from './components/User/Notification';
+import RankList from './components/User/RankList';
+LogBox.ignoreAllLogs();
 
 const Stack = createNativeStackNavigator();
 const App = () => {
-  const [User, setUser] = React.useState(null)
-  const [loader,setLoader]= React.useState(true)
+  const [User, setUser] = React.useState(null);
+  const [loader, setLoader] = React.useState(true);
   const theme = {
     ...DefaultTheme,
     roundness: 2,
@@ -30,26 +26,44 @@ const App = () => {
       text: '#000',
       primary: '#DC7633',
     },
-    mode: 'exact'
+    mode: 'exact',
   };
-  
-  
+
   return (
     <PaperProvider theme={theme}>
       <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Post" component={Post} options={{ headerShown: false }} />
-                <Stack.Screen name="LogIn" component={LogIn} options={{ headerShown: false }} />
-                <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
-                <Stack.Screen name="Forget" component={Forget} options={{ headerShown: false }} />
-                <Stack.Screen name="Rank List" component={RankList}/>
-                <Stack.Screen name="Notification" component={Notification}/>
-                <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Post"
+            component={Post}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="LogIn"
+            component={LogIn}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Forget"
+            component={Forget}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen name="Rank List" component={RankList} />
+          <Stack.Screen name="Notification" component={Notification} />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
-  )
-}
+  );
+};
 
 export default App;
- 
